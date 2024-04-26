0d1304755977f58a900538c88091c5365a05d9f0
YBRrrr33 <61530444+YBRrrr33@users.noreply.github.com>
2024-04-26 20:20:04 +0800

    Add files via upload

--- defangIP.py
+++ defangIP.py
@@ -5,8 +5,8 @@ def ip_address(address):
     #so we first split whatever the address from the user
     split_address = address.split(".")
    # print(split_address) ["1","10","1","8"],and then we join it
-    new_address = split_address.join()
-
+    new_address = "[.]".join(split_address)
     return new_address
 
 ipaddress = ip_address(input("type in your ip address: "))
+print(ipaddress)
